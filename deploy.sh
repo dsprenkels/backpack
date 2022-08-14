7aa1180c245b459b086a968bbebc03ff119d6c4b
Amber Sprenkels <amber@electricdusk.com>
2022-08-14 20:59:25 +0200

    deploy.sh: s/ds7s.nl/as8.nl/

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ export NODE_ENV="production"
 export PUBLIC_URL="/paklijst"
 
 npm run-script build || exit 1
-rsync -ravuzh --delete build/ suki:/var/www/ds7s.nl/paklijst/ || exit 1
+rsync -ravuzh --delete build/ suki:/var/www/as8.nl/paklijst/ || exit 1
