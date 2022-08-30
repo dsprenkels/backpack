f77d4d5f0bf36ea2d026b5855ea2f68fa8c7f3f4
Amber Sprenkels <amber@electricdusk.com>
2022-08-30 17:01:21 +0200

    deploy: Move to /backpack

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export NODE_ENV="production"
-export PUBLIC_URL="/paklijst"
+export PUBLIC_URL="/backpack"
 
 npm run-script build || exit 1
-rsync -ravuzh --delete build/ suki:/var/www/as8.nl/paklijst/ || exit 1
+rsync -ravuzh --delete build/ suki:/var/www/as8.nl/backpack/ || exit 1
