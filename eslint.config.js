223ae1a3c701cd477c470914189bfd75b1534db8
Amber Sprenkels <amber@electricdusk.com>
2025-06-29 21:04:01 +0200

    Move repo out of web/ dir

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,17 @@
-eslint.config.js
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+    { ignores: ["dist/**"] },
+    { languageOptions: { globals: globals.browser } },
+    { settings: { react: { version: "detect" } } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    reactPlugin.configs.flat.recommended,
+    reactPlugin.configs.flat['jsx-runtime'],
+];
